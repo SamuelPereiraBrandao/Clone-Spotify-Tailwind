dd868ac5d6786b0a74481d43edbfff3c070e2aae
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-10-11 16:51:33 -0300

    add

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
+  <NavBar/>
 <!--   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav> -->
   <router-view/>
 </template>
-
+  <script>
+  import NavBar from '@/components/NavBar.vue';
+  export default {
+      components: { NavBar }
+  }
+  </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+<div>ae</div>
+</template>
+<script>
+export default {
+    name: "HomeVue",
+   
+}
+</script>

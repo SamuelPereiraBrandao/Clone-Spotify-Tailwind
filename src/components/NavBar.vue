aac8961fd2c48fb08d78f11744208bdc8ed01e3b
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-10-11 16:15:18 -0300

    add

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template lang="">
+  
+        <div class="flex fixed bg-black w-full px-3 md:px-20 py-4 text-white">
+            <div >
+                <a href="">Home</a>
+                <img src="@/assets/css/img/spotify-logo.png" class="w-20 md:w-125px transition-all" alt="">
+            </div>
+            <div>
+                <i class="fas fa-bars"></i>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="">Premium</a></li>
+                        <li><a href="">Ajuda</a></li>
+                        <li><a href="">Baixar</a></li>
+                        <li><span>
+
+                        </span></li>
+                        <li><a href="">Inscrever-se</a></li>
+                        <li><a href="">Log In</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+  
+</template>

06a08999e78e5d0af2267d15754964d19249405d
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-10-11 16:38:24 -0300

    add

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,31 @@
 <template lang="">
   
-        <div class="flex fixed bg-black w-full px-3 md:px-20 py-4 text-white">
-            <div >
-                <a href="">Home</a>
-                <img src="@/assets/css/img/spotify-logo.png" class="w-20 md:w-125px transition-all" alt="">
-            </div>
-            <div>
-                <i class="fas fa-bars"></i>
-            </div>
-            <div>
-                <nav>
-                    <ul>
-                        <li><a href="">Premium</a></li>
-                        <li><a href="">Ajuda</a></li>
-                        <li><a href="">Baixar</a></li>
-                        <li><span>
+        <div class="flex fixed bg-black w-full px-3 md:px-20 py-4 text-white font-bold	">
+            <div class="container flex items-center mx-auto">
 
-                        </span></li>
-                        <li><a href="">Inscrever-se</a></li>
-                        <li><a href="">Log In</a></li>
-                    </ul>
-                </nav>
+                <div >
+                    <a href=""></a>
+                    <img src="@/assets/css/img/spotify-logo.png" class="w-20 md:w-125px transition-all" alt="">
+                </div>
+                <div class="flex justify-end flex-1 md:hidden text-white text-3xl transition-all">
+                    <i class="fas fa-bars"></i>
+                </div>
+                <div class="items-end flex-1 text-white font bold hidden md:flex trasition-all">
+                    <nav class="flex-1">
+                        <ul class="flex justify-end flex-1">
+                            <li class="px-4"><a class="hover:text-hoverspt text-sm" href="">Premium</a></li>
+                            <li class="px-4"><a class="hover:text-hoverspt text-sm" href="">Ajuda</a></li>
+                            <li class="px-4"><a class="hover:text-hoverspt text-sm" href="">Baixar</a></li>
+                            <li class="px-4"><span class="border-r border-white">
+    
+                            </span></li>
+                            <li class="px-4"><a class="hover:text-hoverspt text-sm" href="">Inscrever-se</a></li>
+                            <li class="px-4"><a class="hover:text-hoverspt text-sm" href="">Log In</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
+
         </div>
   
 </template>
